671447fcc219fc1a2df8fa9bc6b3744e9f19c36b
G-Lumia <giorgiolumia@gmail.com>
2023-03-14 21:07:09 +0100

    added thumbnails, effects and fixed styles

--- js/myscript.js
+++ js/myscript.js
@@ -14,29 +14,39 @@ const images = [
     "img/05.webp"
 ];
 
-const slider = document.querySelector(".slider");
+const slider = document.querySelector(".slides");
+const thumber = document.querySelector(".thumbs");
 
 let currentIndex = 0;
 let slides = " ";
+let thumbs = " ";
 for ( let i=0; i<images.length; i++)
 {
     slides += ` <div class="slide">
                     <img src="${images[i]}" alt="img${i + 1}">
-              </div> ` 
+               </div> ` ;
+
+    thumbs +=  ` <div class="thumb">
+                     <img src="${images[i]}" alt="img${i + 1}">
+                </div> ` ;
 }
 
 slider.innerHTML += slides;
+thumber.innerHTML += thumbs;
+
 document.querySelectorAll(".slide")[currentIndex].classList.add("active");
+document.querySelectorAll(".thumb")[currentIndex].classList.add("active");
 
 const up = document.querySelector(".up");
 const down = document.querySelector(".down");
 
-up.addEventListener("click" , goUp);
+up.addEventListener("click" , goDown);
 
 // funzione per scorrere avanti il vettore di immagini
 
 function goUp() {
     document.querySelectorAll(".slide")[currentIndex].classList.remove("active");
+    document.querySelectorAll(".thumb")[currentIndex].classList.remove("active");
     if(currentIndex == images.length - 1)
     {
         currentIndex = 0;
@@ -46,14 +56,16 @@ function goUp() {
         currentIndex++;
     }
     document.querySelectorAll(".slide")[currentIndex].classList.add("active");
+    document.querySelectorAll(".thumb")[currentIndex].classList.add("active");
 }
 
-down.addEventListener("click" , goDown);
+down.addEventListener("click" , goUp);
 
 // funzione per scorrere indietro il vettore di immagini
 
 function goDown() {
     document.querySelectorAll(".slide")[currentIndex].classList.remove("active");
+    document.querySelectorAll(".thumb")[currentIndex].classList.remove("active");
     if(currentIndex == 0)
     {
         currentIndex = images.length - 1;
@@ -63,4 +75,5 @@ function goDown() {
         currentIndex--;
     }
     document.querySelectorAll(".slide")[currentIndex].classList.add("active");
+    document.querySelectorAll(".thumb")[currentIndex].classList.add("active");
 }
